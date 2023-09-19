a7edf4e7ec149f06c0088346cc6c390b35907761
DanielAmbro <DanielAmbro1@outlook.com>
2023-09-19 18:18:12 -0500

    Add project files.

---
+++ PersonalWebsite/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebsite.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Required")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        public string Subject { get; set; } = null!;
+
+        [Required(ErrorMessage = "* Required")]
+        [DataType(DataType.MultilineText)]
+        [StringLength(500)]
+        public string Message { get; set; } = null!;
+
+       
+    }
+}
